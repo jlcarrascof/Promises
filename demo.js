fa5d31146ce97dfc23de05b6e6752f804a980cf7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-14 11:30:29 -0400

    Testing asynchronous tasks

--- demo.js
+++ demo.js
@@ -3,4 +3,13 @@ function suma(a, b) {
 }
 
 console.log(suma(5, 3));
- 
+
+function operaciones(fn) {
+    setTimeout(() => {
+        console.log(fn(5, 3));
+    }, 2000);
+    console.log('Holiii 😀');
+    console.log('Buenas 🙂');
+}
+
+operaciones(suma);
